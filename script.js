42b86d33016a4fcf8a97bcb68e9cd93fb7a96186
plhnt26-lgtm <plhnt26@gmail.com>
2026-07-20 12:35:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-import { auth, db } from "./firebase.js";
 
-import {
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-signOut,
-onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
-
-import {
-doc,
-setDoc,
-getDoc
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
